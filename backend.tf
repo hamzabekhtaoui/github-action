a0a87ff16732045c66b8f6372cb224632e10f451
hamza94 <hamzabekhtaoui1994@gmail.com>
2024-11-26 17:06:35 +0100

    add backend s3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hb-ippon-tfstate-bucket"
+    bucket         = "hb-ippon-tfstate-bucket-1"
     key            = "github-action" # Check this
     region         = "us-east-1"
     #dynamodb_table = "fr-ippon-formation-terraform-tps-common-october-backend-dynamo"
