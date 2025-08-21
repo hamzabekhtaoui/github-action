70b6625f1b3b667e3fa87dc8fad2fa5546f8719f
hamza94 <hamzabekhtaoui1994@gmail.com>
2025-08-21 16:35:43 +0200

    deploy all applications

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "hb-ippon-tfstate-bucket-1"
-    key            = "github-action" # Check this
-    region         = "us-east-1"
-    #dynamodb_table = "fr-ippon-formation-terraform-tps-common-october-backend-dynamo"
-  }
-}
--- main.tf
+++
@@ -1,9 +0,0 @@
-module "s3_bucket" {
-  source      = "./terraform-s3-module"
-  bucket_name = "hb-ippon-my-first-bucket"
-  versioning  = true
-  tags = {
-    Environment = "Development"
-    Owner       = "Hamza"
-  }
-}
